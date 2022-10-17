3b4d72eabc531c52f49c1d3495da5ea2d2fa8acc
Sutou Kouhei <kou@clear-code.com>
2022-10-17 09:40:35 +0900

    Bump version

--- lib/arrow-numo-narray/version.rb
+++ lib/arrow-numo-narray/version.rb
@@ -1,4 +1,4 @@
-# Copyright 2017-2020 Sutou Kouhei <kou@clear-code.com>
+# Copyright 2017-2022 Sutou Kouhei <kou@clear-code.com>
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,5 +13,5 @@
 # limitations under the License.
 
 module ArrowNumoNArray
-  VERSION = "0.0.6"
+  VERSION = "0.0.7"
 end
